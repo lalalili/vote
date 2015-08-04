b978f351571e9344a1b98109ac34ff6f655fcf80
James <lalalili@gmail.com>
2015-08-04 17:57:23 +0800

    調整得獎名單與獎品圖片

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,10 +111,11 @@
         <section id="abouts">
             <div class="container">
                 <div class="section-heading inverse scrollpoint sp-effect3">
-                    <h2>票選期間：2015/11/1 ~ 2015/12/31</h2>
+                    <h1>活動辦法</h1>
+
                     <div class="divider"></div>
-                    <p>注意事項</p>
-                    <p>本活動如有任何未盡事宜，LUXGEN保由修改活動內容，變更，暫停及解釋活動內容之權力，不另行書面通知參加此活動者與得獎者。</p>
+                    <h3>票選期間：2015/11/1 ~ 2015/12/31</h3>
+                    <h4>本活動如有任何未盡事宜，LUXGEN保由修改活動內容，變更，暫停及解釋活動內容之權力，<BR>不另行書面通知參加此活動者與得獎者。</h4>
                 </div>
                 <div class="row">
                     <div class="col-md-10 col-md-push-1 scrollpoint sp-effect3">
@@ -123,8 +124,9 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="about-comment">
-                                            <h3>1. 參加者資料須填寫完整及正確，若因資料不完整導致無法得知得獎訊息，則取消得獎資格</h3>
-                                            <h3>2. 參加本活動之消費者保證提供的資料均為真實且正確，且未冒用或盜用任何第三人之資料。如有資料不實，不正確之情事或資料不全導致聯絡不上均將被取消參加資格。所有抽獎集中獎資格，需經LUXGEN驗證合格方為有效</h3>
+                                            <h4>1. 參加者資料須填寫完整及正確，若因資料不完整導致無法得知得獎訊息，則取消得獎資格</h4>
+                                            <h4>2.
+                                                參加本活動之消費者保證提供的資料均為真實且正確，且未冒用或盜用任何第三人之資料。如有資料不實，不正確之情事或資料不全導致聯絡不上均將被取消參加資格。所有抽獎集中獎資格，需經LUXGEN驗證合格方為有效</h4>
                                         </div>
                                     </div>
                                 </div>
@@ -133,8 +135,10 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="about-comment">
-                                            <h3>3. 本活動將由LUXGEN以電話通知得將訊息，中獎者必須在15天內返回原投票之生活館/服務廠，填妥所有的領獎資料並提供相關證件(身分證正反面影本)方能領取獎品(禮卷)，如逾期或有造假情事LUXGEN有權取消得獎人資格。</h3>
-                                            <h3>4. 本活動贈品限中獎者本人兌換，恕不得要求折換現金或轉換其他等值商品。若中獎者不收受活動贈品時，視同放棄得獎權力，亦不得變更或折換現金</h3>
+                                            <h4>3.
+                                                本活動將由LUXGEN以電話通知得將訊息，中獎者必須在15天內返回原投票之生活館/服務廠，填妥所有的領獎資料並提供相關證件(身分證正反面影本)方能領取獎品(禮卷)，如逾期或有造假情事LUXGEN有權取消得獎人資格。</h4>
+                                            <h4>4.
+                                                本活動贈品限中獎者本人兌換，恕不得要求折換現金或轉換其他等值商品。若中獎者不收受活動贈品時，視同放棄得獎權力，亦不得變更或折換現金</h3>
                                         </div>
                                     </div>
                                 </div>
@@ -143,8 +147,9 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="about-comment">
-                                            <h3>5. 參加者須同意接受本活動之活動辦法與注意事項之規範，如有違反，LUXGEN得取消其參加或得獎資格。</h3>
-                                            <h3>6. 依中華民國稅法規定，主辦單位將依發開具扣繳憑單給中獎者，贈與物品的價值將併入中獎人的個人綜合所得稅申報。若所得總額超過NT 1,001元，將於隔年寄發機會中獎扣繳憑單(得獎者需提供身分證影本)，若得獎者不願意提供資料，則視同放棄中獎資格，不另行通知，亦不進行候補。</h3>
+                                            <h4>5. 參加者須同意接受本活動之活動辦法與注意事項之規範，如有違反，LUXGEN得取消其參加或得獎資格。</h4>
+                                            <h4>6. 依中華民國稅法規定，主辦單位將依發開具扣繳憑單給中獎者，贈與物品的價值將併入中獎人的個人綜合所得稅申報。若所得總額超過NT
+                                                1,001元，將於隔年寄發機會中獎扣繳憑單(得獎者需提供身分證影本)，若得獎者不願意提供資料，則視同放棄中獎資格，不另行通知，亦不進行候補。</h4>
                                         </div>
                                     </div>
                                 </div>
@@ -158,11 +163,12 @@
             <div class="container">
 
                 <div class="section-heading scrollpoint sp-effect3">
+                    <h1>獎品一覽</h1>
+
                     <div class="divider"></div>
-                    <h3>抽獎時間：2015/12/10(三)，於2015/12/11(四)起，電話通知得獎人</h3>
-                    <h3>抽獎時間：2016/01/10(三)，於2016/12/11(四)起，電話通知得獎人</h3>
-                    <div class="divider"></div>
-                    <p>誠摯地邀請您為微笑服務展現最價的服務/銷售人員一個鼓勵，<BR>請您於投票頁面點選最佳微笑的服務人員，並填寫相關資訊，即可參加抽獎活動</p>
+                    <h4>抽獎時間：2015/12/10(三)，於2015/12/11(四)起，電話通知得獎人</h4>
+                    <h4>抽獎時間：2016/01/10(三)，於2016/12/11(四)起，電話通知得獎人</h4>
+                    <h4>誠摯地邀請您為微笑服務展現最價的服務/銷售人員一個鼓勵，<BR>請您於投票頁面點選最佳微笑的服務人員，並填寫相關資訊，即可參加抽獎活動</h4>
                 </div>
 
                 <div class="row">
@@ -213,41 +219,70 @@
                 <div class="section-heading scrollpoint sp-effect3">
                     <h1>得獎名單</h1>
                 </div>
-
-                <div class="filter scrollpoint sp-effect3">
-                    <a href="javascript:void(0)" class="button js-filter-all active">All Screens</a>
-                    <a href="javascript:void(0)" class="button js-filter-one">User Access</a>
-                    <a href="javascript:void(0)" class="button js-filter-two">Social Network</a>
-                    <a href="javascript:void(0)" class="button js-filter-three">Media Players</a>
+                <div class="one">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"> 第一輪</div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Mark2</td>
+                                        <td>Otto2</td>
+                                        <td>@mdo2</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="slider filtering scrollpoint sp-effect5">
-                    <div class="one">
-                        <img src="{{url('/img/freeze/screens/profile.jpg')}}" alt="">
-                        <h4>Profile Page</h4>
-                    </div>
-                    <div class="two">
-                        <img src="{{url('/img/freeze/screens/menu.jpg')}}" alt="">
-                        <h4>Toggel Menu</h4>
-                    </div>
-                    <div class="three">
-                        <img src="{{url('/img/freeze/screens/weather.jpg')}}" alt="">
-                        <h4>Weather Forcast</h4>
-                    </div>
-                    <div class="one">
-                        <img src="{{url('/img/freeze/screens/signup.jpg')}}" alt="">
-                        <h4>Sign Up</h4>
-                    </div>
-                    <div class="one">
-                        <img src="{{url('/img/freeze/screens/calendar.jpg')}}" alt="">
-                        <h4>Event Calendar</h4>
-                    </div>
-                    <div class="two">
-                        <img src="{{url('/img/freeze/screens/options.jpg')}}" alt="">
-                        <h4>Some Options</h4>
-                    </div>
-                    <div class="three">
-                        <img src="{{url('/img/freeze/screens/sales.jpg')}}" alt="">
-                        <h4>Sales Analysis</h4>
+                <div class="two">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"> 第二輪</div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Mark2</td>
+                                        <td>Otto2</td>
+                                        <td>@mdo2</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
