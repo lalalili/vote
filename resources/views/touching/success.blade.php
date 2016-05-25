35437494b83b96a2d5a6e5025363b53f1c627edb
James <lalalili@gmail.com>
2016-05-25 12:33:51 +0800

    感動服務感謝頁改版

--- resources/views/touching/success.blade.php
+++ resources/views/touching/success.blade.php
@@ -1,139 +1,85 @@
-<!doctype html>
-<!--[if lt IE 7]>
-<html class="no-js ie6"><![endif]-->
-<!--[if IE 7]>
-<html class="no-js ie7"><![endif]-->
-<!--[if IE 8]>
-<html class="no-js ie8"><![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-
+<!DOCTYPE html>
+<html lang="utf8">
 <head>
-    <meta charset="UTF-8">
-    <title>感動服務票選活動</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- Bootstrap 3.3.2 -->
-    <link rel="stylesheet" href="{{ url('/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ url('/css/font-awesome.min.css')}}">
 
-    <link rel="stylesheet" href="{{ url('/css/animate.min.css')}}">
-    <link rel="stylesheet" href="{{ url('/css/slick.min.css')}}">
-    <link rel="stylesheet" href="{{ url('/css/jquery.fullPage.min.css')}}">
+    <title>Cover Template for Bootstrap</title>
 
-    {{--<link rel="stylesheet" href="{{url('/css/styles.css')}}">--}}
-    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{{ url('/touching/css/bootstrap.css') }}">
 
-    <link rel="stylesheet" href="{{ url('/js/rs-plugin/css/settings.css')}}">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link rel="stylesheet" href="{{ url('/touching/css/ie10-viewport-bug-workaround.css') }}">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{ url('/touching/css/cover.css') }}">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
 
-<script src="{{ url('/js/jquery-1.11.3.min.js')}}"></script>
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-<div class="pre-loader">
-    <div class="load-con">
-        <img src="{{url('/images/freeze/logo.png')}}" class="animated fadeInDown" alt="">
+<div class="site-wrapper">
 
-        <div class="spinner">
-            <div class="bounce1"></div>
-            <div class="bounce2"></div>
-            <div class="bounce3"></div>
-        </div>
-    </div>
-</div>
-<header>
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1">
-                    <span class="fa fa-bars fa-lg"></span>
-                </button>
-                <a class="navbar-brand">
-                    <img src="{{url('/images/freeze/logo.png')}}" alt="" class="logo">
-                </a>
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <div class="masthead-brand">
+                        <img src="{{url('/images/freeze/logo.png')}}" alt="" class="logo">
+                    </div>
+
+                    {{--<h3 class="masthead-brand"></h3>--}}
+                    {{--<nav>--}}
+                    {{--<ul class="nav masthead-nav">--}}
+                    {{--<li class="active"><a href="#">Home</a></li>--}}
+                    {{--<li><a href="#">Features</a></li>--}}
+                    {{--<li><a href="#">Contact</a></li>--}}
+                    {{--</ul>--}}
+                    {{--</nav>--}}
+                </div>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            {{--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">--}}
-            {{--<ul class="nav navbar-nav navbar-right">--}}
-            {{--<li><a href="#abouts">活動辦法</a>--}}
-            {{--</li>--}}
-            {{--<li><a href="#prize">獎品一覽</a>--}}
-            {{--</li>--}}
-            {{--<li><a href="#lists">得獎名單</a>--}}
-            {{--</li>--}}
-            {{--</ul>--}}
+
+            <div class="inner cover">
+                <h1 class="cover-heading">投票完成</h1>
+                <h1 class="cover-heading">感謝您的寶貴時間</h1>
+                {{--<p class="lead">感謝您的寶貴時間</p>--}}
+                {{--<p class="lead">--}}
+                {{--<a href="#" class="btn btn-lg btn-default">Learn more</a>--}}
+                {{--</p>--}}
+            </div>
+
+            {{--<div class="mastfoot">--}}
+            {{--<div class="inner">--}}
+            {{--<p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>--}}
             {{--</div>--}}
-                    <!-- /.navbar-collapse -->
+            {{--</div>--}}
+
         </div>
-        <!-- /.container-->
-    </nav>
-<!--RevSlider-->
-<div class="tp-banner-container">
-    <div class="tp-banner">
-        <ul>
-            <!-- SLIDE  -->
-            <li data-transition="fade" data-slotamount="7" data-masterspeed="1500">
-                <!-- MAIN IMAGE -->
-                <img src="{{url('/images/transparent.png')}}" alt="slidebg1" data-bgfit="cover"
-                     data-bgposition="left top" data-bgrepeat="no-repeat">
-                <!-- LAYERS -->
-                <!-- LAYER NR. 1 -->
-                {{--<div class="tp-caption lfl fadeout hidden-xs"--}}
-                {{--data-x="left"--}}
-                {{--data-y="bottom"--}}
-                {{--data-hoffset="30"--}}
-                {{--data-voffset="0"--}}
-                {{--data-speed="500"--}}
-                {{--data-start="700"--}}
-                {{--data-easing="Power4.easeOut">--}}
-                {{--<img src="{{url('/images/freeze/Slides/thumb.png')}}" alt="">--}}
-                {{--</div>--}}
-                {{--<div class="tp-caption lfl fadeout visible-xs"--}}
-                {{--data-x="left"--}}
-                {{--data-y="center"--}}
-                {{--data-hoffset="700"--}}
-                {{--data-voffset="0"--}}
-                {{--data-speed="500"--}}
-                {{--data-start="700"--}}
-                {{--data-easing="Power4.easeOut">--}}
-                {{--<img src="{{url('/images/freeze/Slides/thumb_s.png')}}" alt="">--}}
-                {{--</div>--}}
-                <div class="tp-caption large_white_bold sft" data-x="550" data-y="center" data-hoffset="0"
-                     data-voffset="-80" data-speed="500" data-start="1200" data-easing="Power4.easeOut">
-                    投票完成
-                </div>
-                <div class="tp-caption large_white_light sfb" data-x="550" data-y="center" data-hoffset="0"
-                     data-voffset="0" data-speed="1000" data-start="1500" data-easing="Power4.easeOut">
-                    感謝您的寶貴時間
-                </div>
-            </li>
-        </ul>
+
     </div>
+
 </div>
-</header>
-<script src="{{ url('/js/bootstrap.min.js') }}"></script>
-<script src="{{ url('/js/slick.min.js') }}"></script>
-<script src="{{ url('/js/placeholdem.min.js') }}"></script>
-<script src="{{ url('/js/rs-plugin/js/jquery.themepunch.plugins.min.js') }}"></script>
-<script src="{{ url('/js/rs-plugin/js/jquery.themepunch.revolution.min.js') }}"></script>
-<script src="{{ url('/js/waypoints.min.js') }}"></script>
-<script src="{{ url('/js/modernizr.custom.32033.min.js')}}"></script>
-{{--<script src="{{url('/js/scripts.js')}}"></script>--}}
-<script src="{{ elixir('js/app.js') }}"></script>
 
-<script>
-    $(document).ready(function () {
-        appMaster.preLoader();
-    });
-</script>
-
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="{{ url('/touching/js/jquery.min.js') }}"></script>
+<script>window.jQuery || document.write('<script src="{{ url('/touching/js/jquery.min.js') }}"><\/script>')</script>
+<script type="text/javascript" src="{{ url('/touching/js/bootstrap.min.js') }}"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script type="text/javascript" src="{{ url('/touching/js/ie10-viewport-bug-workaround.js') }}"></script>
 </body>
 </html>
